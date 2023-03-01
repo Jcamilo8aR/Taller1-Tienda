8ed384f68aa85024562434162f000f6d998f6c8b
Jcamilo8aR <jc.ochoar23@gmail.com>
2023-02-28 19:41:24 -0500

    Semana 3 - Arrays

--- src/controllers/controladorHome.js
+++ src/controllers/controladorHome.js
@@ -1,8 +1,27 @@
-//DEFINIENDO DATOS
+import {productos} from "../helpers/baseDatos.js"
 
-let nombre="asd adsads"
-if(5>2){
-    let nombre="HHH"
-}
+// LLAMANDO A LA FILA
+let fila=document.getElementById("fila")
 
-console.log("Hola "+nombre)
+// RECORRIENDO EL ARREGLO
+productos.forEach(function(producto){  // FUNCION ANONIMA (FUNCION SIN NOMBRE) - SE EJECUTA UNA SOLA VEZ
+    
+    // CREANDO COLUMNAS PARA ALOJAR LOS PRODUCTOS
+    let columna = document.createElement("div")
+    columna.classList.add("col")
+
+    // CREAR TARJETA PARA ALMACENAR LA INFO DEL  (USANDO BOOTSTRAP)
+    let tarjeta = document.createElement("div")
+    tarjeta.classList.add("card","h-100","shadow")
+
+    // CREAR UNA IMAGEN
+    let imagen = document.createElement("img")
+    imagen.classList.add("img-fluid", "w-100")
+    imagen.src=producto.fotos[0]
+
+    // PADRES E HIJOS
+    tarjeta.appendChild(imagen)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+
+})  
---
+++ src/helpers/baseDatos.js
@@ -0,0 +1,46 @@
+// QUEMAR UNA BASE DE DATOS
+
+export let productos = Array(
+    {
+     nombre: "The Witcher 3",
+     descripcion: "asd",
+     fotos: ["na1","na2"],
+     precio: 100000,
+     popularidad: 4.5
+    },
+    {
+     nombre: "Red Dead Redemption 2",
+     descripcion: "asd",
+     fotos: ["na1","na2"],
+     precio: 120000,
+     popularidad: 4.7
+    },
+    {
+     nombre: "Assassins Creed II",
+     descripcion: "asd",
+     fotos: ["https://firebasestorage.googleapis.com/v0/b/jcortienda.appspot.com/o/AC2_cover.jpg?alt=media&token=607951e3-f16d-4bce-9dc9-26946d011152","na2"],
+     precio: 80000,
+     popularidad: 4.2
+    },
+    {
+     nombre: "Gran Turismo 7",
+     descripcion: "asd",
+     fotos: ["na1","na2"],
+     precio: 220000,
+     popularidad: 3.6
+    },
+    {
+     nombre: "Grand Theft Auto V",
+     descripcion: "asd",
+     fotos: ["na1","na2"],
+     precio: 120000,
+     popularidad: 4.7
+    },
+    {
+     nombre: "Elden Ring",
+     descripcion: "asd",
+     fotos: ["na1","na2"],
+     precio: 200000,
+     popularidad: 4.9
+    },
+)
